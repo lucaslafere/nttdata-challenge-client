200ced16bffc72899f9d547becee62fbfa03c439
Lucas Lafèré <laferelucas@gmail.com>
2023-03-08 18:33:34 -0300

    feat: added reset scss, global scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+import { ThemeProvider } from "@ui5/webcomponents-react";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
+import "./index.scss";
 import { store } from "./store/store";
-import { ThemeProvider } from "@ui5/webcomponents-react";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
