6a49b3fa9bb1091ee64b4f6699d2e6165e9a4536
Lucas Lafèré <laferelucas@gmail.com>
2023-03-08 18:23:38 -0300

    chore: added ui5 components lib

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./store/store";
+import { ThemeProvider } from "@ui5/webcomponents-react";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
