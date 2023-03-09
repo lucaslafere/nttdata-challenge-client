c3f5ecdf2108a8fa5abbb742251b225957791695
Lucas Lafèré <laferelucas@gmail.com>
2023-03-09 03:02:59 -0300

    refactor(style): add better mobile ux

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,21 +3,24 @@ import { FC } from "react";
 import "./style.scss";
 
 interface iProps {
-  children: string;
+  children?: string;
   onClick: () => void;
   disabled?: boolean;
+  icon?: string;
 }
 
 export const ClickableButton: FC<iProps> = ({
   children,
   onClick,
   disabled,
+  icon,
 }) => {
   return (
     <Button
       className="orange-button"
       onClick={onClick}
       disabled={disabled}
+      icon={icon}
     >
       {children}
     </Button>
